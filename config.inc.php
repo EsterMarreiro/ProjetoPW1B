a14762aab6d1879af3ebb232c766700dfe5c6678
Ester Marreiro <estermarreiro@outlook.com>
2024-11-02 17:14:29 -0300

    alteracao do usuario, senha e banco de dados do arquivo config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-  $conexao = mysqli_connect("localhost","daniel","654321");
+  $conexao = mysqli_connect("localhost","equipe_e","000000");
 
-  $bd = mysqli_select_db($conexao,"bd2");
+  $bd = mysqli_select_db($conexao,"bd_equipe_e");
