f5707b63e00c2b416b3e9efc4a55dca4684659df
Ester Marreiro <estermarreiro@outlook.com>
2024-11-06 15:50:15 -0300

    adicao de uma condicional para informar se a mensagem foi apagada ou se houve falha; fechamento da tag de escopo do php

--- admin/excluir.php
+++ admin/excluir.php
@@ -6,6 +6,12 @@
 
     $sql = mysqli_query($conexao,"DELETE FROM contatos WHERE id = $id");
 
+    if($sql){
+        echo "<h2>Mensagem apagada com sucesso</h2>";
+    } else {
+        echo "<h2>Falha na exclusão</h2>";
+    }
+
     mysqli_close($conexao);
 
-    echo "<h2>Mensagem excluída.</h2>";
+?>
