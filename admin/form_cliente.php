1f7c9fa127fe67ca830802eda244f795119f4541
Ester Marreiro <estermarreiro@outlook.com>
2024-11-09 15:41:58 -0300

    criacao do arquivo form_cliente.php para exibir um formulario para receber os dados para o cadastro dos clientes

---
+++ admin/form_cliente.php
@@ -0,0 +1,10 @@
+<h3>Cadastro</h3>
+
+<form action="?pg=cadastra_cliente" method="post">
+  Nome: <input type="text" name="nome"> <br>
+  E-mail: <input type="text" name="email"> <br>
+  Telefone: <input type="text" name="telefone"> <br>
+  Cidade: <textarea name="cidade"> </textarea><br>
+  Estado: <input type="text" name="estado"> <br>
+ <input type="submit" value="Cadastrar">
+</form>
