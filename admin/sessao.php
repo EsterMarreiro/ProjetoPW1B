ecbeeccd36228337f9d2298c5b91d2e3921a6aca
Lucas Leandro <lucasfariasleandro@gmail.com>
2024-11-13 12:55:12 -0300

    Organização da base do projeto

--- admin/sessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-    function logado() {
-        session_start();    //existe o session destroy que faz o logout, encerra a sessao
-        return isset($_SESSION['user_id']);
-    }
-
-?>
--- config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  $conexao = mysqli_connect("localhost","equipe_e","000000");
-
-  $bd = mysqli_select_db($conexao,"bd_equipe_e");
